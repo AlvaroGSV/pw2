a7673ca96173455d978eebf6121a0a884e9bf956
yeratzy <yeratzyen@gmail.com>
2020-11-11 14:52:36 -0700

    Crud solo con front

--- VUE-06/todos/src/store/index.js
+++ VUE-06/todos/src/store/index.js
@@ -6,26 +6,48 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     todos: [],
+    todo: {
+      id: '',
+      nombre: '',
+      tipos: [],
+      prioridad: '',
+      horas: 0
+    }
   },
   mutations: {
-    SET_TODO(state, todo){
+    SET_TODOS(state, todo){
       state.todos.push(todo)
     },
     DELETE_TODO(state, id){
       state.todos = state.todos.filter(todo => todo.id !== id)
+    },
+    SET_TODO(state, id){
+      state.todo = state.todos.find(todo => todo.id === id)
+    },
+    UPDATE_TODO(state, todo){
+      state.todos = state.todos.map(t => t.id === todo.id ? todo : t)
     }
   },
   actions: {
-    setTodo({ commit }, todo){
-      commit('SET_TODO', todo)
+    setTodos({ commit }, todo){
+      commit('SET_TODOS', todo)
     },
     deleteTodo({ commit }, id) {
       commit('DELETE_TODO', id)
+    },
+    setTodo({commit}, id){
+      commit('SET_TODO', id)
+    },
+    updateTodo({commit}, todo) {
+      commit('UPDATE_TODO', todo)
     }
   },
   getters: {
     allTodos: state => {
       return state.todos
+    },
+    singleTodo: state => {
+      return state.todo
     }
   },
   modules: {
--- VUE-06/todos/src/views/EditarTodo.vue
+++ VUE-06/todos/src/views/EditarTodo.vue
@@ -1,13 +1,90 @@
 <template>
   <div>
       <h3>Editar todo</h3>
-      {{$route.params.id}}
+     <form @submit.prevent="guardarTodo">
+      <div class="form-group">
+          <label for="InputNombre">Nombre</label>
+          <input v-model.trim="todo.nombre" type="text" class="form-control" id="InputNombre" placeholder="Ingrese nombre">
+      </div>
+      <hr>
+      <div>Tipos</div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.tipos" class="form-check-input" type="checkbox" id="inlineCheckbox1" value="Hogar">
+        <label class="form-check-label" for="inlineCheckbox1">Hogar</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input  v-model="todo.tipos" class="form-check-input" type="checkbox" id="inlineCheckbox2" value="Escuela">
+        <label class="form-check-label" for="inlineCheckbox2">Escuela</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.tipos" class="form-check-input" type="checkbox" id="inlineCheckbox3" value="Oficina">
+        <label class="form-check-label" for="inlineCheckbox3">Oficina</label>
+      </div>
+      <hr>
+      <div>Prioridad</div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.prioridad" class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Baja">
+        <label class="form-check-label" for="inlineRadio1">Baja</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.prioridad" class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Media">
+        <label class="form-check-label" for="inlineRadio2">Media</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.prioridad" class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="Alta">
+        <label class="form-check-label" for="inlineRadio3">Alta</label>
+      </div>
+      <hr>
+      <div class="form-group">
+          <label for="InputHoras">Horas requeridas</label>
+          <input v-model.number="todo.horas" type="number" class="form-control" id="InputHoras" disabled>
+      </div>
+      <button type="submit" class="btn btn-success" :disabled="bloquearBoton">Guardar</button>
+    </form>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
-    name: 'EditarTodo'
+    name: 'EditarTodo',
+    data() {
+      return {
+        todo: {}
+      }
+    },
+    computed: {
+      ...mapGetters(['singleTodo']),
+      bloquearBoton(){
+      return this.todo.nombre.trim() === "" ? true : false
+    }
+    },
+    methods: {
+      ...mapActions(['setTodo']),
+      guardarTodo(){
+      
+
+        this.limpiarTodo();
+        this.$router.push({ name: 'Home'});
+    },
+    limpiarTodo(){
+      this.todo = {
+        id: '',
+        nombre: '',
+        tipos: [],
+        prioridad: '',
+        horas: 0
+      }
+    }
+    },
+    created() {
+      this.setTodo(this.$route.params.id)
+    },
+    beforeMount(){
+      Vue.set(this, 'todo', this.singleTodo )
+    }
 
 }
 </script>
