1b1607856d60d1eb4855daa31b51e90a25d00074
yeratzy <yeratzyen@gmail.com>
2020-11-17 15:00:11 -0700

    Rutas de agregar, editar y eliminar

--- node-mysql/src/routes/personasRoute.js
+++ node-mysql/src/routes/personasRoute.js
@@ -10,4 +10,10 @@ routes.get('/', personasController.listar);
 
 routes.get('/:id', personasController.obtenerPersona );
 
+routes.post('/', personasController.crear )
+
+routes.put('/:id', personasController.editar)
+
+routes.delete('/:id', personasController.eliminar )
+
 module.exports = routes;
